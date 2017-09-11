6719e3f80a532afe22eeb08cab05e38edc29e8ab
sohugebird <song02211368@outlook.com>
2017-09-11 23:38:20 +0800

    end

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-console.log(location.href );
-console.log(location.href );
-console.log(location.href );
-console.log(location.href );
-console.log(location.href );
+function Student(name) { 
+    this.name = this.name;
+}
+Student.prototype.hello = function () { 
+    alert('Hello' + this.name + '!')
+}
+
+var vedio = new Student();
+console.log(vedio.constructor === Student.prototype.constructor);
